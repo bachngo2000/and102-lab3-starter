29ef58b4f818f3dc485cf56aec3e84f614dd1bc1
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2024-03-13 23:44:22 -0500

    connected the front-end XML to the back-end Kotlin.

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
@@ -3,8 +3,10 @@ package com.codepath.bestsellerlistapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.codepath.bestsellerlistapp.R.id
 
 /**
@@ -28,10 +30,14 @@ class BestSellerBooksRecyclerViewAdapter(
      * This inner class lets us refer to all the different View elements
      * (Yes, the same ones as in the XML layout files!)
      */
+    // we'll use an inner class to keep track of all those Views in fragment_best_seller_book.xml:
     inner class BookViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
         var mItem: BestSellerBook? = null
         val mBookTitle: TextView = mView.findViewById<View>(id.book_title) as TextView
         val mBookAuthor: TextView = mView.findViewById<View>(id.book_author) as TextView
+        val mBookRanking: TextView = mView.findViewById<View>(id.ranking) as TextView
+        val mBookDescription: TextView = mView.findViewById<View>(id.book_description) as TextView
+        val mBookImage: ImageView = mView.findViewById<View>(id.book_image) as ImageView
 
         override fun toString(): String {
             return mBookTitle.toString() + " '" + mBookAuthor.text + "'"
@@ -41,12 +47,24 @@ class BestSellerBooksRecyclerViewAdapter(
     /**
      * This lets us "bind" each Views in the ViewHolder to its' actual data!
      */
+    // next, populate the Views with data from a book:
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
         val book = books[position]
 
         holder.mItem = book
         holder.mBookTitle.text = book.title
         holder.mBookAuthor.text = book.author
+        // Set the description and rank Views' text fields
+        holder.mBookDescription.text = book.description
+        // TextViews require Strings, but "rank" is an integer. We can use rank.toString() to get around this.
+        holder.mBookRanking.text = book.rank.toString()
+
+        // To load the image, we'll use the Glide library.
+        // This takes the image from the given URL, loads it, and stores it into the ImageView
+        Glide.with(holder.mView)
+            .load(book.bookImageUrl)
+            .centerInside()
+            .into(holder.mBookImage)
 
         holder.mView.setOnClickListener {
             holder.mItem?.let { book ->
