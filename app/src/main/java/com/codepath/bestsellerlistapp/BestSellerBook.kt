16efa22d0ef76aa360ee1cc8d4b6b277411a613e
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2024-03-13 22:56:56 -0500

    created the model

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBook.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBook.kt
@@ -10,6 +10,7 @@ import com.google.gson.annotations.SerializedName
  */
 // the model representing a single book
 class BestSellerBook {
+    // @SerializedName(String) tags are used by the gson library to select which JSON key to import data from. Make sure they match the returned json exactly!
     @SerializedName("rank")
     var rank = 0
 
@@ -21,11 +22,15 @@ class BestSellerBook {
     @SerializedName("author")
     var author: String? = null
 
-    //TODO bookImageUrl
+    // Create a String? variable called bookImageUrl that will match the correct JSON key.
+    @SerializedName("book_image")
+    var bookImageUrl: String? = null
 
+    //Create a String? variable called description that will match the correct JSON key.
+    @SerializedName("description")
+    var description: String? = null
 
-    //TODO description
-
-
-    //TODO-STRETCH-GOALS amazonUrl
+    // TODO-STRETCH-GOALS amazonUrl
+    @SerializedName("amazon_product_url")
+    var amazonProductUrl: String? = null
 }
